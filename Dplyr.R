13da38c83d4dd3ac83c29262a33dd3cccf898080
Denys Kostiuk <denyskostiuk@uk-gn-36531.local>
2017-05-28 19:11:04 +0300

    Filter, spread

--- Dplyr.R
+++ Dplyr.R
@@ -1,4 +1,5 @@
 library("dplyr")
+library(tidyr)
 
 x <- tbl_df(iris)
 glimpse(iris)
@@ -9,3 +10,16 @@ iris %>%
   arrange(desc(Sepal.Length)) %>%
   summarise(fst = first(Sepal.Length))
 
+iris %>% 
+  spread(Species,Sepal.Width)
+
+
+stocks <- data.frame(
+  time = as.Date('2009-01-01') + 0:9,
+  X = rnorm(10, 0, 1),
+  Y = rnorm(10, 0, 2),
+  Z = rnorm(10, 0, 4)
+)
+stocksm <- stocks %>% gather(stock, price, -time)
+stocksm %>% spread(stock, price)
+stocksm %>% spread(time, price)
