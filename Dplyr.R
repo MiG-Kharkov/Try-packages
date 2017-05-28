87b93bd2fdacdf0798301906dab620631539e32a
Denys Kostiuk <denyskostiuk@uk-gn-36531.local>
2017-05-28 19:13:20 +0300

    Select with inclution more than n times

--- Dplyr.R
+++ Dplyr.R
@@ -1,5 +1,6 @@
 library("dplyr")
 library(tidyr)
+library(stringr)
 
 x <- tbl_df(iris)
 glimpse(iris)
@@ -10,16 +11,24 @@ iris %>%
   arrange(desc(Sepal.Length)) %>%
   summarise(fst = first(Sepal.Length))
 
-iris %>% 
-  spread(Species,Sepal.Width)
+x<- cbind(ID = 1:150, iris)
+x %>% 
+  select(ID, Sepal.Length, Species) %>%
+  spread(Species, Sepal.Length, sep = "+") ->y
 
+y %>% 
+  unite(ID, `Species+setosa`, `Species+versicolor`, `Species+virginica`, sep  = "-")
 
-stocks <- data.frame(
-  time = as.Date('2009-01-01') + 0:9,
-  X = rnorm(10, 0, 1),
-  Y = rnorm(10, 0, 2),
-  Z = rnorm(10, 0, 4)
-)
+stocks <- data.frame(time = as.Date('2009-01-01') + 0:9,
+  X = rnorm(10, 0, 1),  Y = rnorm(10, 0, 2),  Z = rnorm(10, 0, 4))
 stocksm <- stocks %>% gather(stock, price, -time)
 stocksm %>% spread(stock, price)
 stocksm %>% spread(time, price)
+
+iris %>%
+  filter(Species == "setosa", Petal.Length > 1.8, Sepal.Length == 4.8)
+
+iris %>%
+  group_by(Petal.Width) %>%
+  summarise(nwifth = n()) %>%
+  filter(nwifth > 3)
