f4c0ec7ef1eda7f18decfe46dc9c0fe15bfaf738
Igor Manzhos <manzhos.igor@gmail.com>
2017-05-30 07:34:43 +0300

    Facet_grid added

--- ggplot2.R
+++ ggplot2.R
@@ -7,3 +7,12 @@ x <- iris
 iris %>%
   ggplot(aes(Sepal.Length, Sepal.Width))+
   geom_point(aes(color = Species, shape = Species))
+
+iris %>%
+  ggplot(aes(Species, Sepal.Length))+
+  geom_boxplot()
+
+iris %>%
+  ggplot(aes(Sepal.Length, Sepal.Width))+
+  geom_point()+
+  facet_grid(Species~.)
